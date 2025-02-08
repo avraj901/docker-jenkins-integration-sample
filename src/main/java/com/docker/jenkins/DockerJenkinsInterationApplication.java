4d281c4c3f4d3aaa7cf3c4bf373837237dfee928
avraj901 <avraj901@gmail.com>
2025-02-08 11:19:26 +0530

    Added code for docker jenkins integration

---
+++ src/main/java/com/docker/jenkins/DockerJenkinsInterationApplication.java
@@ -0,0 +1,13 @@
+package com.docker.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJenkinsInterationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJenkinsInterationApplication.class, args);
+	}
+
+}
